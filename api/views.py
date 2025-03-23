59fe7a236d80ba432d58be27c3a697e0e45910b6
Victoran0 <victoranosunkvic@gmail.com>
2025-03-23 01:56:32 +0100

    handle requests from users by generating a response from the get_agent_response function

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from .agent import get_agent_response
 
 # Create your views here.
 
@@ -11,14 +12,21 @@ class ChatViewSet(viewsets.ViewSet):
     permission_classes = [AllowAny]
 
     def create(self, request):
-        """handle requests from users and generate a response"""
-        if not request.data["body"]:
-            return Response({"response": "request must have a body"}, status=status.HTTP_400_BAD_REQUEST)
+        """handle requests from users and generate a response by calling get_agent_response"""
+        # use .get to prevent KeyError
+        if not request.data.get("body"):
+            return Response({"response": "request must have a 'body' field"}, status=status.HTTP_400_BAD_REQUEST)
 
         user_req = request.data["body"]
         print("The user's request: ", user_req)
+        try:
+            agent_response = get_agent_response(user_req)
+            print("The agent response: ", agent_response)
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            return Response({"error": "Am error occured while processing your request"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        return Response({"message": "Received successfully"}, status=status.HTTP_200_OK)
+        return Response({"agent_response": agent_response}, status=status.HTTP_200_OK)
 
     def list(self, request, *args, **kwargs):
-        return Response({"message": "Send a post request with a body"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return Response({"message": "Send a POST request with a body"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)

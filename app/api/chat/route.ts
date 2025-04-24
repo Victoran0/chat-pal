33e46e341820172df85b708d7dee6c5d50552c6b
Victoran0 <victoranosunkvic@gmail.com>
2025-04-24 03:57:40 +0100

    remove  deepgram text to speech API calls

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -29,36 +29,38 @@ export async function POST(request: NextRequest) {
     text = agentFinalState.messages[agentFinalState.messages.length - 1].content.toString()
     // console.log("The AI AGENT response: ", text)
 
+    return new Response(JSON.stringify(text), {status: 200})
+
   } catch (error: any) {
     console.error("The AI AGENT error: ", error)
     return new Response(JSON.stringify(error?.response), {status: 500})
   }
 
-  try {
-    // gotta use the request object to invalidate the cache every request :vomit:
-    // const url = request.url;
-    const deepgram = createClient(process.env.DEEPGRAM_API_KEY ?? "");
+  // try {
+  //   // gotta use the request object to invalidate the cache every request :vomit:
+  //   // const url = request.url;
+  //   const deepgram = createClient(process.env.DEEPGRAM_API_KEY ?? "");
 
-    const model = request.nextUrl.searchParams.get("model") ?? "aura-asteria-en";
+  //   const model = request.nextUrl.searchParams.get("model") ?? "aura-asteria-en";
 
-    console.log("model: ", model, "\nThe AI AGENT response: ", text);
+  //   console.log("model: ", model, "\nThe AI AGENT response: ", text);
 
-    const result = await deepgram.speak.request({text: text}, { model });
-    const stream = await result.getStream();
-    const headers = await result.getHeaders();
+  //   const result = await deepgram.speak.request({text: text}, { model });
+  //   const stream = await result.getStream();
+  //   const headers = await result.getHeaders();
 
-    const response = new NextResponse(stream, { headers });
-    response.headers.set("Surrogate-Control", "no-store");
-    response.headers.set(
-      "Cache-Control",
-      "s-maxage=0, no-store, no-cache, must-revalidate, proxy-revalidate",
-    );
-    response.headers.set("Expires", "0");
+  //   const response = new NextResponse(stream, { headers });
+  //   response.headers.set("Surrogate-Control", "no-store");
+  //   response.headers.set(
+  //     "Cache-Control",
+  //     "s-maxage=0, no-store, no-cache, must-revalidate, proxy-revalidate",
+  //   );
+  //   response.headers.set("Expires", "0");
 
-    return response;
-  } catch (error: any) {
-      console.error("The backend error: ", error)
-      return new Response(JSON.stringify(error?.response), {status: 500})
-  }
+  //   return response;
+  // } catch (error: any) {
+  //     console.error("The backend error: ", error)
+  //     return new Response(JSON.stringify(error?.response), {status: 500})
+  // }
 
 }

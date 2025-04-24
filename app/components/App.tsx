c5077283165fa0eedff243b41f93ad16dc7567bb
Victoran0 <victoranosunkvic@gmail.com>
2025-04-24 23:18:18 +0100

    Add play button to enable user interaction before Chat Pal starts speaking, to enable playback support on iOS Browsers

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -6,11 +6,14 @@ import TapToSpeak from './TapToSpeak';
 import SpeechToText from './SpeechToText';
 import { useChatPalStore } from '@/providers/chatpal-store-provider';
 import RippleLoader from './RippleLoader';
+import { Play } from 'lucide-react';
+import UseTextToSpeech from './TextToSpeech';
 
 const App = () => {
     const [context, setContext] = useState<AudioContext>();
     const [audioUrl, setAudioUrl] = useState<string>("");
-    const { isListening, getResponse, isLoading, refreshSTTCount, chatPalResponse, isConnecting } = useChatPalStore((state) => state,)
+    const { isListening, getResponse, isLoading, refreshSTTCount, chatPalResponse, toggleBoolean, isSpeaking } = useChatPalStore((state) => state,)
+    const {textToSpeech} = UseTextToSpeech()
 
     return (
         <>
@@ -22,8 +25,25 @@ const App = () => {
                 />
             )}
             {chatPalResponse !== "" && !isLoading && (
-                <RippleLoader />
+                isSpeaking ? <RippleLoader /> : (
+                    <div className="flex items-center font-[600] flex-col gap-3 justify-center">
+                        <button 
+                            type='button' 
+                            title='Listen to Chat Pak Response'
+                            onClick={() => {
+                                textToSpeech(chatPalResponse)
+                                toggleBoolean("isSpeaking", true)  
+                            }}
+                        >
+                            <Play size={100} fill='#13EF93' color='#13EF93' className='sm:max-w-none bg-gradient-to-r from-[#149AFB] via-[#13EF93] bg-[length:200%_200%] to-[#149AFB] bg-clip-text text-transparent hover:scale-110 transition-all duration-150 active:scale-50 animate-gradient' />
+                        </button>
+                        <p className='sm:max-w-none bg-gradient-to-r from-[#149AFB] via-[#13EF93] bg-[length:200%_200%] to-[#149AFB] bg-clip-text text-transparent animate-gradient'>
+                            press play to listen
+                        </p>
+                    </div>
+                )
             )}
+
             <AnimatePresence>
                 {!isListening && !getResponse && (
                     <TapToSpeak />
